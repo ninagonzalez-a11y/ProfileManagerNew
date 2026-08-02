f7f9554f17b5f787187ec00b9592354f1b54a0f2
Nina <nina_gonzalez@dlsu.edu.ph>
2026-08-02 15:33:10 +0800

    Fix Spring Boot main method

--- src/main/java/ph/edu/dlsu/lbycpob/profilemanagerapplication/ProfileManagerApplication.java
+++ src/main/java/ph/edu/dlsu/lbycpob/profilemanagerapplication/ProfileManagerApplication.java
@@ -2,10 +2,11 @@ package ph.edu.dlsu.lbycpob.profilemanagerapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class ProfileManagerApplication {
-    static void main(String[] args) {
+
+    public static void main(String[] args) {
         SpringApplication.run(ProfileManagerApplication.class, args);
     }
 }
-
